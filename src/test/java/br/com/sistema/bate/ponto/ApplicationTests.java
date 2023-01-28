ba48fb5ded99cabcef10135f16b3fd3168cc8a6b
thalysw <thalys.welson@gmail.com>
2023-01-28 07:51:18 -0300

    new project

---
+++ src/test/java/br/com/sistema/bate/ponto/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistema.bate.ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
